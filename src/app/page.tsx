8f66caec71a3661954e520859ab83fe3601dbcc3
Júlio Caria <julio.caria@hotmail.com>
2023-10-24 23:17:45 -0300

    feat: add auth handler to header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+"use client"
+
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return (
-    <div></div>
-  )
+  const { data } = useSession();
+  return <div></div>
 }
